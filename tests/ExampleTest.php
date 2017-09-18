b55f6b4647bd09392c0981c306481fee28033622
Ashley Clarke <me@ashleyclarke.me>
2017-09-18 20:04:21 +0200

    first

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_works()
+    {
+        Assert::assertTrue(true);
+    }
+}
